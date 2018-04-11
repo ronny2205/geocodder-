691beeea89b38930cf56c9bf18dc2f32ff177114
Ronny Almog <ronny2205@gmail.com>
2018-04-10 20:52:28 -0400

    creating a new addresss

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,17 @@
 class AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+    @address = Address.new
   end
+
+  def create
+    @address = Address.create(address_params)
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:street, :city, :state, :country, :zipcode)
+  end
+
 end
