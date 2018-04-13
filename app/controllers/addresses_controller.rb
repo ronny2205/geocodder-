e8001c7401458fd2e0a5898af5dacbf362720571
Ronny Almog <ronny2205@gmail.com>
2018-04-12 20:25:32 -0400

    adding a state dropdown

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -13,26 +13,43 @@ class AddressesController < ApplicationController
 
   def create
     @address = Address.new(address_params) #can be avoided?
+
+    if @address.invalid?
+      flash[:error] = 'Could not save. the data you entered is invalid.'
+    end
+
     full_address = @address.full_address #(address_params)
     #response = HTTParty.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDNG0ihVr0FUxlzquiVvInxvPPTwRRdXa8')
-  response = HTTParty.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + full_address + '&key=AIzaSyDNG0ihVr0FUxlzquiVvInxvPPTwRRdXa8')
+  
+  #response = HTTParty.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + full_address + '&key=AIzaSyDNG0ihVr0FUxlzquiVvInxvPPTwRRdXa8')
   
     #response = HTTParty.get('https://maps.googleapis.com/maps/api/geocode/json?address=49 Kuku St, Kfar Saba Israel&key=AIzaSyDNG0ihVr0FUxlzquiVvInxvPPTwRRdXa8')
 
 
-    results = response.parsed_response['results']
+    results =[] #response.parsed_response['results']
   
-  #puts results
   if !results.empty?
-    @address.latitude = results[0].dig('geometry', 'location', 'lat')
-    @address.longitude = results[0].dig('geometry', 'location', 'lng')
+    @address.latitude = 1 #results[0].dig('geometry', 'location', 'lat')
+    @address.longitude = -2 #results[0].dig('geometry', 'location', 'lng')
     @address.save
+
+    if !@address.valid?
+      render :index, status: :unprocessable_entity
+    end
+
+    if @address.valid?
+      # success msg
+
+      redirect_to root_path
+    end
   else
     @no_address_msg = 'Can not geocode this address. Please try again.'
+    p @no_address_msg
+    redirect_to root_path(@no_address_msg)
+
   end
-  
+
    #Address.create(address_params)
-    redirect_to root_path
   end
 
   private
